a1f52565e3a7a34a595edbfef5a895061c623ce8
ruiduque <rmduque@icloud.com>
2018-12-17 14:47:52 +0000

    Final version of run_analysis

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# har_repo
-Human Activity Recognition using Smartphones 
+
+# Human Activity Recognition using Smartphones Project
+### by: Rui Santos | Dec-2018
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
+
+
+This project uses a public domain dataset consisting of daily live observations of 30 subjects acquired on a smartphone.
+
+Original project homepage: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+
+
+File catalog:
+
+1. es2013-84.pdf - Academic paper describing the experiment and dataset
+2. run_analysis.R - Analysis script to...
+3. UCI HAR Dataset - Original dataset
+
+
+References:
+Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
---
+++ run_analysis.R
@@ -0,0 +1,82 @@
+##
+## Human Activity Recognition Using Smartphones Dataset
+##
+
+library(readr)
+library(dplyr)
+library(stringr)
+
+# Dataset folder locations
+repoloc <- "/Users/ruisantos/Git/har_repo/"
+maindatasetfolder <- "./UCI HAR Dataset/"
+testfolder <- paste(maindatasetfolder,"test/", sep = "")
+trainfolder <- paste(maindatasetfolder,"train/", sep = "")
+
+# Set project folder
+setwd(repoloc)
+
+# File locations
+actlabelloc <- paste(maindatasetfolder,"activity_labels.txt", sep = "")
+featloc <- paste(maindatasetfolder,"features.txt", sep = "")
+subtest <- paste(testfolder, "subject_test.txt", sep = "")
+subtrain <- paste(trainfolder, "subject_train.txt", sep = "")
+test_y_labels <- paste(testfolder, "y_test.txt", sep = "")
+train_y_labels <- paste(trainfolder, "y_train.txt", sep = "")
+x_test <- paste(testfolder, "X_test.txt", sep = "")
+x_train <- paste(trainfolder, "X_train.txt", sep = "")
+
+## Read files
+activitylabels <- readr::read_delim(actlabelloc, delim = " ", col_names = c("activity", "activity_label"))
+features <- readr::read_delim(featloc, delim = " ", col_names = c("feature", "feature_label"))
+
+# Test folder files:
+subject_test <- readr::read_delim(subtest, delim = " ", col_names = "subject")
+y_test_labels <- readr::read_delim(test_y_labels, delim = " ", col_names = "activity_id")
+x_test_df <- readr::read_table(x_test, col_names = as.character(features$feature_label))
+
+
+# Train folder files:
+subject_train <- readr::read_delim(subtrain, delim = " ", col_names = "subject")
+y_train_labels <- readr::read_delim(train_y_labels, delim = " ", col_names = "activity_id")
+x_train_df <- readr::read_table(x_train, col_names = as.character(features$feature_label))
+
+# Add subject, activity labels and measurments to the same data-frame
+test_data <- cbind(subject_test, y_test_labels,
+                   activity = sapply(y_test_labels$activity_id, function(x) as.character(activitylabels[x,2])),
+                   x_test_df)
+
+train_data <- cbind(subject_train, y_train_labels,
+                    activity = sapply(y_train_labels$activity_id, function(x) as.character(activitylabels[x,2])),
+                    x_train_df)
+
+##
+## 1. Merges the training and the test sets to create one data set.
+##
+one_dataset <- as_tibble(rbind(test_data, train_data))
+
+##
+## 2. Extracts only the measurements on the mean - "mean()" and standard deviation - "std()" for each measurement.
+##      - Note: keeps subject & activity as these will be required later
+##
+mean_std_df <- as_tibble(cbind(one_dataset[,"subject"], one_dataset[,"activity"], one_dataset[,stringr::str_detect(names(one_dataset), "mean\\(\\)|std\\(\\)" )]))
+
+##
+## 3. Uses descriptive activity names to name the activities in the data set
+##      - Note: most of the variables have already been set with descriptive names. 
+
+
+##
+## 4. Appropriately labels the data set with descriptive variable names.
+##      - Note: only changing six variables which seem to have wrong names 
+##              e.g. "fBodyBodyGyroJerkMag-std()" to "fBodyGyroJerkMag-std()"      
+names(mean_std_df) <- stringr::str_replace(names(mean_std_df), "BodyBody", "Body")
+
+##
+## 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity
+## and each subject.
+##
+mean_tidy <- mean_std_df %>%
+        group_by(activity, subject) %>%
+        summarise_all(mean, na.rm = TRUE)
+
+
